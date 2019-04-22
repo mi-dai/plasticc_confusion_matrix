3cf1de955c982807fb58133deaeaf720e3d590f8
Renee Hlozek <renee.hlozek@gmail.com>
2019-04-22 14:53:42 -0400

    updated reordering of cm

--- gen_confusion_matrix.py
+++ gen_confusion_matrix.py
@@ -10,7 +10,7 @@ from sklearn.metrics import confusion_matrix
 import pandas as pd
 import sys
 
-def plot_confusion_matrix(cm, classes,
+def plot_confusion_matrix(cm, classes, newinds=[],
                           normalize=False,
                           title='Confusion matrix',
                           cmap=plt.cm.Blues):
@@ -18,6 +18,27 @@ def plot_confusion_matrix(cm, classes,
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
     """
+
+
+    if newinds==[]:
+        newinds= np.arange(len(classes))
+
+    print(newinds, 'newinds', len(newinds), 'length', len(classes), 'class length')
+    newcm = [[0] * len(classes) for i in range(len(classes))]
+
+    for ci,i in enumerate(newinds):
+        for cj,j in enumerate(newinds):
+            newcm[ci][cj]=cm[i][j]
+
+    newcm = np.array(newcm)
+#    print(newcm[0][0], cm[newinds[0]][newinds[0]])
+    print(np.shape(newcm), 'newcm shape')
+
+    print(np.shape(cm), 'cm shape')
+
+    cm= newcm
+
+
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         print("Normalized confusion matrix")
@@ -26,12 +47,18 @@ def plot_confusion_matrix(cm, classes,
 
     print(cm)
 
+
+
+
+
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
     tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+    print(classes,'original')
+    print(classes[newinds],'newinds')
+    #plt.xticks(tick_marks, classes[newinds], rotation=45)
+    #plt.yticks(tick_marks, classes[newinds])
 
     fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
@@ -50,19 +77,31 @@ def main():
     # read in data
     submission_file = sys.argv[1]
     filenum = submission_file.split('.csv')[0]
-    truth_file = "plasticc_test_truthtable.csv"
-    classname_file = "classnames.txt"
-    z_file = "redshifts.tsv"
+    truth_file = "truth/plasticc_test_truthtable.csv"
+    classname_file = "truth/classnames.txt"
+    z_file = "truth/redshifts.tsv"
     classlist = [6,15,16,42,52,53,62,64,65,67,88,90,92,95,99]
+    
+    newclasslist = [15,16,53,92,65,42,62,6,90,88,64,52,67,95,99]
+
+    classlist = newclasslist
+    newinds = [1,2,5,12,8,3,6,0,11,10,7,4,9,13,14]
+
     if len(sys.argv)<4:
         zrange = [-np.inf,np.inf]
     else:
         zrange = [np.float(sys.argv[2]),np.float(sys.argv[3])]
 
     classnames = pd.read_table(classname_file,sep='\s+',header=0)
+    print(classnames)
     classnames = classnames.sort_values(by='TARGET')
+    print(classnames, 'sorted')
     classnames = classnames['MODEL_NAME']
+    print(classnames,'modelname')
 
+    print(newinds)
+    classnames = classnames[newinds]
+    print(classnames, 'check')
     ztable = pd.read_csv(z_file,sep='\t',header=0)
     ztable.rename(columns={'SNID':'object_id'},inplace=True)
     ztable = ztable[(ztable['SIM_REDSHIFT_HOST']>zrange[0]) & (ztable['SIM_REDSHIFT_HOST']<=zrange[1])]
@@ -80,10 +119,11 @@ def main():
     truth.sort_values(by='object_id',inplace=True)
 
     classids = [x for x in list(probs) if x[0].isdigit()]
+    print(classids,'classids')
     y_pred = probs[classids].idxmax(axis=1).values.astype(int)
-    print(y_pred[0:10])
+#    print(y_pred[0:10])
     y_true = truth['target'].values
-    print(y_true[0:10])
+ #   print(y_true[0:10])
 
     # Compute confusion matrix
     cnf_matrix = confusion_matrix(y_true, y_pred)
@@ -103,7 +143,8 @@ def main():
     # Plot normalized confusion matrix
     fig = plt.figure(figsize=(10,10))
 
-    plot_confusion_matrix(cnf_matrix, classes=classes, normalize=True,
+    inds = newinds
+    plot_confusion_matrix(cnf_matrix, classes=classes, newinds=inds,normalize=True,
                           title='Normalized confusion matrix-z{}_{}-{}'.format(zrange[0],zrange[1],filenum))
     fig.savefig("{}_z{}_{}.pdf".format(submission_file,zrange[0],zrange[1]))
 
